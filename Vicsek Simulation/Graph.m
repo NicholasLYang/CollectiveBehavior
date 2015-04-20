dc588ab0c383968b3ca1e3dcbd4ecc21229596e8
Nicholas Yang <nick@nicholasyang.com>
2015-04-20 16:31:49 -0400

    Added a more advanced form of noise

--- Vicsek Simulation/Graph.m
+++ Vicsek Simulation/Graph.m
@@ -1,16 +1,18 @@
 
-function [milling, polarization] = Graph(agents, sizeOfGraph, repulsionConst, frames, speed, fps, eta)
+function [milling, polarization, cohesion] = Graph(agents, agentSize, sizeOfGraph, repulsionConst, frames, speed, fps, eta, alpha, lambda, gamma)
 % Gives a total number of agents for the for loop
 numOfAgents = size(agents);
 % Graphs the agents using a quiver plot (adds arrows instead of dots)
+
 polarization = zeros(frames);
 milling = zeros(frames);
-
-
+cohesion = zeros(frames);
+o = zeros(numOfAgents(2));
 for j = 1:frames
 
    p = [0 0];  
    m = [0 0 0];
+   c = 0;
    % loops through the entire array, changing coordinates and direction
    for x = 1:numOfAgents(2)
        angle = agents(3, x);
@@ -30,25 +32,29 @@ for j = 1:frames
     % f is the total sum of all the repulsion vectors.
           f1 = nCor - coords;
     % Remember, f1 is a vectior pointing from the agent to the neighbor
-          r = (1 + exp(norm(f1)/(sizeOfGraph * 0.1) - 2))^-1;
-          e = (-f1)/norm(f1); 
-          if f1 == 0
-              e = [0, 0];
-          end
-          f = f + r * e;
+
+          
            distance = norm(f1);
-           if distance < sizeOfGraph/10
+          if distance < sizeOfGraph
+           r = (1 + exp(norm(f1)/(sizeOfGraph * 0.1) - 2))^-1;
+           e = (-f1)/norm(f1); 
+           if f1 == 0
+             e = [0, 0];
+           end
+           f = f + r * e; 
+          end
+           if distance < sizeOfGraph/2
                
                nAngle = agents(3, y);
                
                    
 
                % Basic Vicsek:
-              % c = dot(direction, f1)/(norm(f1) * norm(direction));
-               %if f1 == 0
-                %   c = 0;
-               %end
-               sumOfDirections = sumOfDirections + nAngle; % * (1 + c) ;
+               cosine = dot(direction, f1)/(norm(f1) * norm(direction));
+               if f1 == 0
+                  cosine = 0;
+               end
+               sumOfDirections = sumOfDirections + nAngle * (1 + cosine) ;
                numOfNeighbors = numOfNeighbors + 1;
 
                
@@ -59,14 +65,22 @@ for j = 1:frames
                %}
            end
        end
+       randNum = rand();
+       if randNum < lambda
+           r = 0;
+       end
+           r = randn();
+     
+       o(x) = (1 - alpha) * o(x) + randn() + gamma * r;
+     
+
       % Add on repulsion constant
        f = f * repulsionConst;
       % This is extrinsic noise, noise which comes from "free will"
       % agents(3,x) = mod(sumOfDirections / numOfNeighbors + rand() * pi * (1 - 2 * rand()), 2 * pi); 
       % This is intrinsic noise 
-      % fprintf('Angle 1 is %d\n', agents(3, x));
-       angle = mod(eta * rand() * 2 * pi + sumOfDirections / numOfNeighbors, 2 * pi);
-      % fprintf('Angle 2 is %d\n', agents(3, x));
+       angle = mod(eta * o(x) + sumOfDirections / numOfNeighbors, 2 * pi);
+
        % Chate noise
       % agents(3,x) = mod( sumOfDirections / numOfNeighbors + 2 * pi * eta * real(exp(1i * rand())), 2 * pi);
        
@@ -74,26 +88,32 @@ for j = 1:frames
       % agents(3,x) = mod(sumOfDirections/numOfNeighbors, 2 * pi);
       % agents(1, x) = mod(speed * cos(agents(3, x)) + agents(1, x), sizeOfGraph);
       % agents(2, x) = mod(speed * sin(agents(3, x)) + agents(2, x), sizeOfGraph);
-     % More advanced with repulsion
+     % With repulsion
      agents(1, x) = mod(speed * cos(agents(3,x)) + f(1) + agents(1, x), sizeOfGraph);
      agents(2, x) = mod(speed * sin(agents(3,x)) + f(2) + agents(2, x), sizeOfGraph);
      agents(3, x) = angle;
      direction = [cos(angle) sin(angle)];
      % fprintf('Agent has a repulsion of %d\n', f(1));
+     % More advanced noise
+ 
      % fprintf('Agent %d has xcor of %d, a ycor of %d and a direction of %d\n', x, agents(1, x), agents(2, x), agents(3,x)); 
     
      p = p + direction;
      m = m + cross([coords 0], [direction 0])/(norm(coords) * speed);
-      
+     c = c + exp(-norm(coords)/(2 * sizeOfGraph));
    end
+ 
     polarization(j) = norm(p) / numOfAgents(2);
     milling(j) = norm(m)/ numOfAgents(2);    
-      
-    % quiver(agents(1,:), agents(2, :), cos(agents(3,:)), sin(agents(3,:)));
-    % axis([0,sizeOfGraph,0,sizeOfGraph]);
-    
+    cohesion(j) = c / numOfAgents(2); 
+     quiver(agents(1,:), agents(2, :), cos(agents(3,:)), sin(agents(3,:)), agentSize);
+     axis([0,sizeOfGraph,0,sizeOfGraph]);
+    %{
     fprintf('Polarization is %d\n', polarization(j));
     fprintf('Milling is %d\n', milling(j));
+    fprintf('Cohesion is %d\n', cohesion(j))
+    %}
+    fprintf('%d\n',j);
     pause(1/fps)
     drawnow
 end
--- Vicsek Simulation/Process.m
+++ Vicsek Simulation/Process.m
@@ -1,10 +1,22 @@
+% Graph(agents, agentSize sizeOfGraph, repulsionConst, frames, speed, fps, eta, alpha, lambda, gamma)
+frames = 100;
+agents = AddAgents(100, 20);
+[m, p, c] = Graph(agents, 0.25, 20, 0.1, frames, 1, 30, 0.1, 0.20, 0.5 , pi );
 
-agents = AddAgents(250, 10);
-[m, p] = Graph(agents, 15, 0, 100, 1, 10, 0);
 
-subplot(2,1,1);
+
+subplot(3,1,1);
 plot(m);
+axis([0,frames, 0, 1]);
 title('Milling');
-subplot(2,1,2);
+subplot(3,1,2);
 plot(p)
+axis([0,frames, 0, 1]);
 title('Polarization');
+subplot(3,1,3);
+plot(c)
+axis([0,frames,0,1]);
+title('Cohesion');
+
+
+
